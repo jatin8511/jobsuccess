b7bfbbd89da9826af4ab6ef11b5335fe5f8a8531
jatin8511 <105959808+jatin8511@users.noreply.github.com>
2023-05-18 13:13:52 +0530

    .

--- jobsuccess/backend/model/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const UserChema = mongoose.Schema({
-    name: String,
-    email: String,
-    msg: String,
-})
-
-const User = mongoose.model('contact',UserChema)
-module.exports = User
